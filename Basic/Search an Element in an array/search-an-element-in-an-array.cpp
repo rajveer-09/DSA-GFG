3d4254306c65a079083c616214d2be1933923a21
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-11 16:37:02 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Search an Element in an array/search-an-element-in-an-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+         for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == X)
+            {
+                return i;  // Return the index of the element found, not the element itself
+            }
+        }
+        return -1;  // Return -1 if the element is not found in the array
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+    
+}
+
+// } Driver Code Ends
