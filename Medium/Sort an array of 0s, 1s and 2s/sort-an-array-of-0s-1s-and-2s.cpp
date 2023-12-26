fa6e87754741174c5b33f2389bb7d6438bc4efe3
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-12-26 14:19:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -10,28 +10,8 @@ class Solution
     public:
     void sort012(int arr[], int n)
     {
-        // code here
-        int c0=0,c1=0,c2=0;
-        
-        for(int i=0;i<n;i++){
-            if(arr[i]==0)
-            c0++;
-            if(arr[i]==1)
-            c1++;
-            if(arr[i]==2)
-            c2++;
-        }
-        for(int i=0;i<n;i++){
-            if(i<c0)
-            arr[i]=0;
-            
-            else if(i>=c0&&i<c0+c1)
-            arr[i]=1;
-            
-            else
-            arr[i]=2;
-        }
-        
+        // code here 
+        sort(arr,arr+n);
     }
     
 };
