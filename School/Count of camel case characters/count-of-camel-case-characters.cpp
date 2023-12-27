58a6945c9a5e34b584d031f257bbd1abeee907b4
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-12-27 23:06:39 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count of camel case characters/count-of-camel-case-characters.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+  public:   
+    int countCamelCase (string s)
+    {
+    	//code here.
+    	  int count=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(isupper(s[i]))
+        {
+            count+=1;
+        }
+    }
+    return count;
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout <<ob.countCamelCase (s) << '\n';
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
