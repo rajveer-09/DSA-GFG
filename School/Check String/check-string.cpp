bae114c77a27bd3ba6946bf107957afb012e11e3
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-12-27 22:48:14 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check String/check-string.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        bool check (string s)
+        {
+        	//code here.
+        	for(int i=0;i<s.size()-1;i++){
+        	    
+        	    if(s[i]!=s[i+1])
+        	    return false;
+        	}
+        	
+        	return true;
+        	
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+		string s;
+		cin >> s;
+		Solution ob;
+		if (ob.check (s))
+			cout << "YES\n";
+		else 
+			cout << "NO\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
