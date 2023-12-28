3efd77993d6db4de259a82d9db14d956237139d2
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-12-28 17:40:40 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        for(int i = 0; i < s.length(); i++)
+            for(int j = 1 + i; j <= s.length(); j++)
+                if(s[i] == s[j])
+                    return s[i];
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
