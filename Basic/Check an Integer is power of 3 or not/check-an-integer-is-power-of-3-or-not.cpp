e141c74a6d9a7d3467205f15f89a8b58aa42a00b
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-01 13:44:26 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check an Integer is power of 3 or not/check-an-integer-is-power-of-3-or-not.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isPowerof3(int n){
+        // code here 
+         if(1162261467%n){return "No";}
+          else {return "Yes";}
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isPowerof3(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
