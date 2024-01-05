25f7655c3fad734576a0b2805b04b9585dfa0051
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-05 22:30:43 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
+++ Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -11,7 +11,7 @@ class Solution
     void rotateby90(vector<vector<int> >& matrix, int n) 
     { 
         // code here 
-         for(int i = 0; i<n; i++){
+        for(int i = 0; i<n; i++){
             for(int j = 0; j<i; j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
