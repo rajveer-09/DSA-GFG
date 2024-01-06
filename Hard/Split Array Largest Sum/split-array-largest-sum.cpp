a3def5c138ea1e6643bdfe20f1efe94c0078f44a
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-07 01:13:42 +0530

    Updated Solution - GfG to GitHub

--- Hard/Split Array Largest Sum/split-array-largest-sum.cpp
+++ Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -6,28 +6,62 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int splitArray(int arr[] ,int n, int m) {
-        // code here
-        int start = 0, end = 0;
-        for (int i = 0; i < n; i++) {
-            start = max(start, arr[i]); // the max element
-            end += arr[i]; // the total sum
-        }
-        // Binary search
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            // calculate how many chunks the mid value can make
-            int sum = 0, pieces = 1;
-            for (int i = 0; i < n; i++) {
-                if (sum + arr[i] > mid) {
-                    sum = arr[i];
-                    pieces++;
-                } else sum += arr[i];
+    bool isPossible(int arr[], int n, int m, int mid) {
+    int sum = 0;
+    int subCnt = 1;  // Initialize subCnt to 1 since you are starting a new subarray
+
+    for (int i = 0; i < n; i++) {
+        if (sum + arr[i] <= mid) {
+            sum += arr[i];
+        } else {
+            subCnt++;
+            if (subCnt > m || arr[i] > mid) {
+                return false;
             }
-            if (pieces > m) start = mid + 1; // if the chunks are more than required, increase the mid value
-            else end = mid; // if the chunks are less than or equal to required, decrease the mid value
+            sum = arr[i];
         }
-        return start;
+
+        if (subCnt > m) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int splitArray(int arr[], int n, int K) {
+    int s = 0;
+    int maxi = INT_MIN;
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxi) {
+            maxi = arr[i];
+        }
+    }
+    s = maxi;
+
+    int e = 0;
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    e = sum;
+
+    int ans = 0;
+
+    while (s <= e) {
+        int mid = s + (e - s) / 2;
+
+        if (isPossible(arr, n, K, mid)) {
+            ans = mid;
+            e = mid - 1;
+        } else {
+            s = mid + 1;
+        }
+    }
+
+    return ans;
     }
 };
 
