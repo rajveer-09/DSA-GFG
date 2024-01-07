49626d7a8150137d357d74015acf5c45d374eaf5
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-07 23:36:31 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+
+void append(struct Node** headRef, int newData)
+{
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	struct Node *last = *headRef;
+	new_node->data = newData;
+	new_node->next = NULL;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+
+int sumOfLastN_Nodes(struct Node* head, int n);
+
+// Driver program to test above
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        struct Node* head = NULL;
+        int n,m,tmp;
+        cin>>m>>n;
+        while(m--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        cout<<sumOfLastN_Nodes(head, n)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the node of the linled list is as
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+*/
+// your task is to complete this function 
+// function should return sum of last n nodes
+int sumOfLastN_Nodes(struct Node* head, int n)
+{
+      // Code here
+      int cP = 0;
+    Node* temp = head;
+
+    // Calculate the length of the linked list
+    while (temp != NULL) {
+        temp = temp->next;
+        cP++;
+    }
+
+    // Reset temp to the head
+    temp = head;
+
+    int sum = 0;
+    int c = 0;
+
+    // Traverse the linked list and accumulate the sum of the last n nodes
+    while (temp != NULL) {
+        if (c >= cP - n) {
+            sum += temp->data;
+        }
+        temp = temp->next;
+        c++;
+    }
+
+    return sum;
+      
+}
