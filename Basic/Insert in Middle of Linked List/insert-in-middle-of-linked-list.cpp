24589615e26eb7023dc812723e02124d5a8e71be
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-08 00:17:17 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Insert in Middle of Linked List/insert-in-middle-of-linked-list.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+Node* insertInMiddle(Node* head, int x)
+{
+	// Code here
+	Node* temp=head;
+int count=0;
+while(temp!=NULL){
+    count++;
+    temp=temp->next;
+}
+temp=head;
+int k=count/2;
+ 
+if((count%2)!=0){
+    k=k+1;
+}
+ 
+int kth=0;
+Node* new_node=new Node(x);
+ 
+while(kth!=k-1){
+ 
+    temp=temp->next;
+      kth++;
+}
+ 
+new_node->next=temp->next;
+temp->next=new_node;
+return head;
+ 
+}
