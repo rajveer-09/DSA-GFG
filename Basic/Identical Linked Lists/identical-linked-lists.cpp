51733ed338732fa91703cbceef0afa9553f82276
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-12 00:37:22 +0530

    Updated Solution - GfG to GitHub

--- Basic/Identical Linked Lists/identical-linked-lists.cpp
+++ Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -68,14 +68,13 @@ struct Node {
 bool areIdentical(struct Node *head1, struct Node *head2)
 {
     // Code here
-    while(head1->next!=0&&head2->next!=0){
-        if(head1->data != head2->data) return false;
-        
-        head1=head1->next;
-        head2= head2->next;
+    Node* temp1= head1;
+    Node* temp2= head2;
+    while(temp1!=nullptr && temp2!=nullptr){
+        if(temp1->data != temp2->data)  return 0;
+        temp1=temp1->next;
+        temp2=temp2->next;
     }
-    if(head1->data!=head2->data) return false;
-    if(head1->next!=0||head2->next!=0) return false;
-    
-    return true;
+    if(temp1!=temp2) return 0;
+    return 1;
 }
