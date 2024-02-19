a373db21825d090b770c8cb987da76e2ae98dee3
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-19 20:52:39 +0530

    Updated Solution - GfG to GitHub

--- Easy/Equilibrium Point/equilibrium-point.cpp
+++ Easy/Equilibrium Point/equilibrium-point.cpp
@@ -12,20 +12,32 @@ class Solution{
     int equilibriumPoint(long long a[], int n) {
     
         // Your code here
-         int sum1 = 0;
-        for(int i = 0; i<n; i++)
-        {
-            sum1 = sum1 + a[i];
+         if (n == 1) return 1;
+
+    int low = 0;
+    int high = n - 1;
+    long long sum1 = 0;
+    long long sum2 = 0;
+
+    while (low <= high) {
+        if (sum1 < sum2) {
+            sum1 += a[low];
+            low++;
+        } else if (sum1 > sum2) {
+            sum2 += a[high];
+            high--;
+        } else {
+            if (low == high) {
+                return low + 1; 
+            }
+            sum1 += a[low];
+            sum2 += a[high];
+            low++;
+            high--;
         }
-        int sum2 = 0;
-        for(int i = 0; i<n; i++)
-        {
-            sum1 = sum1 - a[i];
-            if(sum1 == sum2)
-            return i+1;
-            sum2 = sum2 + a[i];
-        }
-        return -1;
+    }
+
+    return -1;
     }
 
 };
