828690b1bf488fd0c31eb0dad99ad63c5e58de40
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-20 16:08:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find Number of Numbers/find-number-of-numbers.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+int num(int a[], int n, int k);
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+		{
+			cin>>a[i];
+		}
+		int k;
+		cin>>k;
+		cout<<num(a,n,k)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function given below*/
+int num(int a[], int n, int k)
+{
+     //add code here.
+     int cnt=0;
+     for(int i=0;i<n;i++){
+        while(a[i]){
+            if(a[i]%10==k) cnt++;
+            a[i]/=10;
+        }
+     }
+     return cnt;
+}
