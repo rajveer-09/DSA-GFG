294e32fe63729efe9929bc381f0c29d6ac9d6d07
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-23 03:02:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        //User function Template for C++
+        
+        int maxProfit(vector<int>&price){
+            //Write your code here..
+            int a=INT_MAX, b=0, c=INT_MAX, d=0;
+            for(auto &x:price){
+                a=min(a,x);
+                b=max(b,x-a);
+                c=min(c,x-b);
+                d=max(d,x-c);
+            }
+            return d;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int> price(n);
+        for(int i=0;i<n;i++) cin>>price[i];
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
+    }
+
+}
+
+// } Driver Code Ends
