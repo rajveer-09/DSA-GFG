5cbed3706697ee2686eca8dc8372c39fc7f9b060
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-24 00:06:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Sum Problem/maximum-sum-problem.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxSum(int n)
+        {
+            //code here.
+            int t[n+1];
+            t[0]=0;
+            for(int i=1;i<=n;i++){
+                t[i]=max(i,t[i/2]+t[i/3]+t[i/4]);
+            }
+            return t[n];
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.maxSum(n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
