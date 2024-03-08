ca780466c54e34fa69a0b7f23b2797e2694817d4
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-08 12:30:27 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reach the Nth point/reach-the-nth-point.cpp
+++ Easy/Reach the Nth point/reach-the-nth-point.cpp
@@ -7,7 +7,7 @@ class Solution{
 	public:
 		int nthPoint(int n){
 		    // Code here
-		      int curr = 2, prev = 1, mod = 1000000007;
+		    int curr = 2, prev = 1, mod = 1000000007;
             
             for(int i=2; i<=n; i++){
                 int temp = (curr+prev)%mod;
