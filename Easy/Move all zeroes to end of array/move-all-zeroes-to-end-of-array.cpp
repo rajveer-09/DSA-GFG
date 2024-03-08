2a8d9ec1958c6c0b5ca2bbae7dd841a163c46dd5
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-08 15:18:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Move all zeroes to end of array/move-all-zeroes-to-end-of-array.cpp
+++ Easy/Move all zeroes to end of array/move-all-zeroes-to-end-of-array.cpp
@@ -9,9 +9,9 @@ public:
 	void pushZerosToEnd(int arr[], int n) {
 	    // code here
 	    int j = 0;
-        for (int i = 0; i < n; i++)
-            if (arr[i] != 0)
-                swap(arr[j++], arr[i]);
+        for (int i = 0; i < n; i++){
+            if (arr[i] != 0) swap(arr[j++], arr[i]);
+	    }
 	}
 };
 
