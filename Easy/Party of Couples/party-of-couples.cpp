c787ca84fa6acf50393436acbf6d498012053dcd
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-04-10 09:57:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        return accumulate(arr, arr + n, 0, [&](int a, int b) -> int {return a ^ b;});
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
