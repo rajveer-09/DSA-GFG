db2f3f54b7b7297ee2bd7d52a0f893f29125d4a3
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-04-23 00:02:12 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+        int dp[n+1];
+        dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
