de819e270388cd41d2fde1ecc383e1ec24f08a2f
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-04-27 02:45:52 +0530

    Updated Solution - GfG to GitHub

--- Basic/Squares in a Matrix/squares-in-a-matrix.cpp
+++ Basic/Squares in a Matrix/squares-in-a-matrix.cpp
@@ -9,14 +9,17 @@ class Solution {
   public:
     long long squaresInMatrix(int m, int n) {
         // code here
-        long long sum=0;
-     while(m>=1&& n>=1){
-         sum+=m*n;
-         m--;
-         n--;
-         
-     }
-     return sum;
+        if(m==0 || n==0){
+
+            return 0;
+
+        }
+
+        else{
+
+            return m*n+squaresInMatrix(m-1,n-1);
+
+        }
     }
 };
 
