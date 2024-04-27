20be683704df8b47efe06f1e868c80bb4191ca68
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-04-28 00:57:55 +0530

    Updated Solution - GfG to GitHub

--- Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
+++ Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
@@ -1,7 +1,6 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h> 
 using namespace std; 
-
 struct Node
 {
     int data;
@@ -13,8 +12,51 @@ struct Node
     }
 };
 
-/* Function to get the middle of the linked list*/
-struct Node *deleteMid(struct Node *head);
+
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        // Your Code Here
+        Node* prev = nullptr;
+        Node* slow= head;
+        Node* fast = head;
+        while(fast->next!= nullptr ){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next;
+            if(fast->next!= nullptr){
+                fast = fast->next;
+            }
+        }
+        prev->next = slow->next;
+        delete(slow);
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
 void printList(Node* node) 
 { 
 	while (node != NULL) { 
@@ -42,7 +84,8 @@ int main()
 			tail->next = new Node(data);
 			tail = tail->next;
 		}
-		head = deleteMid(head);
+		Solution obj;
+		head = obj.deleteMid(head);
 		printList(head); 
 	}
 	return 0; 
@@ -51,41 +94,3 @@ int main()
 
 
 // } Driver Code Ends
-
-
-/* Link list Node:
-
-struct Node
-{
-    int data;
-    struct Node* next;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
-
-*/
-
-// Deletes middle of linked list and returns head of the modified list
-Node* deleteMid(Node* head)
-{
-    // Your Code Here
-       if(!head || !head->next)
-    {
-        return NULL;
-    }
-    Node* curr=head;
-    Node*fast=head;
-    Node*slow=head;
-    Node* prev=NULL;
-    while(fast && fast->next)
-    {
-        fast=fast->next->next;
-        prev=slow;
-        slow=slow->next;
-    }
-    prev->next=slow->next;
-    return curr;
-}
