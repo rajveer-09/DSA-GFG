41cdca0c2f9d30eeff4d56a4155f1b88c535115e
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-05-20 06:03:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.cpp
+++ Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.cpp
@@ -9,17 +9,15 @@ class Solution
 		long long int PowMod(long long int x,long long int n,long long int M)
 		{
 		    // Code here
-		    long long ans= 1;
-            
-            while(n>0){
-                if(n&1){
-                    ans=((ans)%M *(x)%M) %M;
-                }
-                x=((x)%M * (x)%M) %M;
-                n=n>>1;
-            }
-            
-            return ans;
+		   long long res =1;
+           while(n>0){
+               if(n&1){
+                   res = (res*(x)%M)%M; 
+               }
+               x = (((x)%M)*((x)%M))%M;
+               n = n>>1;
+           }
+           return res;
 		}
 };
 
