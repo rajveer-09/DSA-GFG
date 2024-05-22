dd8214e1d9c4e39c5aeb55f6cfd92e79d12ed978
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-05-22 16:23:34 +0530

    Updated Solution - GfG to GitHub

--- Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -2,51 +2,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
-  bool isValid(vector<int>& stations, int K, double distance, int n){
-        int new_stations = 0;
-        for(int i=1; i<n; i++)
-            new_stations += floor((stations[i] - stations[i-1]) / distance);
-            
-        return new_stations <= K;
-    }
   public:
-    double findSmallestMaxDist(vector<int> &stations, int K){
-        int n = stations.size();
-        double start = 0;
-        double end = stations[n-1] - stations[0];
-        while(end - start > 1e-6){
-            double mid = start+(end-start)/2.0;
-            if(isValid(stations, K, mid, n))
-                end = mid;
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        // Code here
+        int n=stations.size();
+        double low=0,high=stations[n-1],ans=1e9;
+        while(high-low>0.00001){
+            double mid = low + (high-low)/2;
+            int count=k;
+            for(int i=1;i<stations.size();i++)
+                count-=max(0,(int)(ceil)(((double)(stations[i]-stations[i-1])/mid))-1);
+            if(count>=0){
+                ans=mid;
+                high=mid;
+            }
             else
-                start = mid;
+                low=mid;
         }
-        
-        return start + 0.000001;
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
-int main()
-{
-  int t;
-  cin >> t;
-  while (t--)
-  {
-    int n;
-    cin >> n;
-    vector<int> stations(n);
-    for (int i = 0; i < n; i++)
-    {
-      cin >> stations[i];
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> stations(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stations[i];
+        }
+        int k;
+        cin >> k;
+        Solution obj;
+        cout << fixed << setprecision(2) << obj.findSmallestMaxDist(stations, k)
+             << endl;
     }
-    int k;
-    cin >> k;
-    Solution obj;
-    cout << fixed << setprecision(2) << obj.findSmallestMaxDist(stations, k) << endl;
-  }
-  return 0;
+    return 0;
 }
 // } Driver Code Ends
