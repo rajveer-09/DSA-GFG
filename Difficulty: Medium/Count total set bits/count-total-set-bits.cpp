0e13ce30e2b462a4fe2e37e8af7070c8e604ba94
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-07-13 00:46:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count total set bits/count-total-set-bits.cpp
+++ Difficulty: Medium/Count total set bits/count-total-set-bits.cpp
@@ -12,30 +12,30 @@ class Solution{
     public:
     // n: input to count the number of set bits
     //Function to return sum of count of set bits in the integers from 1 to n.
-    int bit(int n) {
-    int prev = 1;
-    if (n == 0) return 1;
-    if (n == 1) return prev+1;
-    int curr;
-    for (int i = 1; i < n; i++) {
-        curr = (1 << i) + 2*prev; 
-        prev = curr;
+int countSetBits(int n) {
+    int count = 0;
+    int powerOfTwo = 1;
+
+    while (powerOfTwo <= n) {
+        // Calculate number of complete sets of powerOfTwo * 2
+        int totalPairs = n + 1;
+        int completePairs = totalPairs / (powerOfTwo * 2);
+        
+        // Each pair contributes powerOfTwo set bits
+        count += completePairs * powerOfTwo;
+
+        // Calculate remainder pairs that contribute additional set bits
+        int remainder = totalPairs % (powerOfTwo * 2);
+        if (remainder > powerOfTwo) {
+            count += remainder - powerOfTwo;
+        }
+
+        powerOfTwo <<= 1; // Move to the next power of two
     }
-    return prev+1;
+
+    return count;
 }
 
-int countSetBits(int n) {
-    int ans = 0;
-    int cnt=0;
-    for (int i = 31; i >=0; i--) {
-        if (n & (1 << i)) {
-            ans += bit(i);
-            ans+=(1<<i)*cnt;
-            cnt++;
-        }
-    }
-    return ans;
-}
 };
 
 
