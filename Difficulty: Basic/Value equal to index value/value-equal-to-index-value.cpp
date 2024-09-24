322b7b3df9110d7a21423f83a73a92ef5f2e9749
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-24 19:15:14 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find elements in the array that are equal to their index.
+    vector<int> valueEqualToIndex(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int>a;
+        for(int i=0 ; i<n ; i++){
+            if(arr[i] == i+1){
+                a.push_back(arr[i]);
+            }
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        auto ans = ob.valueEqualToIndex(nums);
+        if (ans.empty()) {
+            cout << "Not Found";
+        } else {
+            for (int x : ans) {
+                cout << x << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
