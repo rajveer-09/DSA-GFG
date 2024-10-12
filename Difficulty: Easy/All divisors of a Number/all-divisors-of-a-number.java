67a83798450ec58ee16460135247263924e5cadf
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-10-12 15:22:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            obj.print_divisors(n);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static void print_divisors(int n) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+                if (i != n / i) {
+                    divisors.add(n / i);
+                }
+            }
+        }
+
+        Collections.sort(divisors);
+        for (int divisor : divisors) {
+            System.out.print(divisor + " ");
+        }
+    }
+}
