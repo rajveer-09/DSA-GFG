6f95b291f5f86fcee79f02742b38f5ea86d31f7e
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-11-06 15:20:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
@@ -4,37 +4,24 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to check if a string can be obtained by rotating
-    //another string by exactly 2 places.
-    bool isRotated(string str1, string str2)
-    {
-        // Your code here
-        int n1=str1.size()-1;
-        int n2= str2.size()-1;
-        
-        if(n1!=n2) return false;
-        
-        if(str2[0]==str1[n1-1]&& str2[1]==str1[n1]){
-            for(int i=0;i<n1-2;i++){
-                if(str1[i]!=str2[i+2]) return false;
-            }
-            
-            return true;
-        }
-         if(str2[n2-1]==str1[0]&& str2[n2]==str1[1]){
-            for(int i=0;i<n1-2;i++){
-                if(str1[i+2]!=str2[i]) return false;
-            }
-            
-            return true;
-        }
-        
-        return false;
-    }
 
+class Solution {
+public:
+    // Function to check if a string can be obtained by rotating
+    // another string by exactly 2 places.
+    bool isRotated(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        
+        if (n1 != n2 || n1 < 2) return false;
+        
+        
+        string clockwise_rot = s1.substr(n1 - 2) + s1.substr(0, n1 - 2);
+        
+        string counterclockwise_rot = s1.substr(2) + s1.substr(0, 2);
+        
+        return (s2 == clockwise_rot || s2 == counterclockwise_rot);
+    }
 };
 
 
