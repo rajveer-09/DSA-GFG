3017b824002f0450673d2f3728ceb3bdea915c12
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-12-02 17:43:36 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> search(string& pat, string& txt) {
+        // code here
+        int n = txt.length();
+        int m = pat.length();
+        
+        vector<int> lps(m, 0);
+        int len = 0;
+        int i = 1;
+        
+        while (i < m) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        
+        vector<int> res;
+        int j = 0; 
+        i = 0; 
+        
+        while (i < n) {
+            if (pat[j] == txt[i]) {
+                i++;
+                j++;
+            }
+            
+            if (j == m) {
+                res.push_back(i - j);
+                j = lps[j - 1];
+            } else if (i < n && pat[j] != txt[i]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector<int> res = ob.search(pat, S);
+        if (res.size() == 0)
+            cout << "[]" << endl;
+        else {
+            for (int i : res)
+                cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
