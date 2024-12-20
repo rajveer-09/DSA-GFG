8953f15b5bd4223d5ec2b14a031fa12932ff94f8
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-12-20 21:24:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -4,55 +4,46 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
-  public:
-    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
-        // code here
-        int n=matrix.size();//row i
-        int m=matrix[0].size();//col j
-        int up=0;
-        int right=m-1;
-        int down=n-1;
-        int left=0;
-        int totcount=m*n;
-        int count=0;
-        vector<int>ans;
-        
-        
-        while(true)
-        {
-            for(int j=left;j<=right;j++)
-            {
-                ans.push_back(matrix[up][j]);
-                count++;
+public:
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        vector<int> result;
+        if (mat.empty() || mat[0].empty()) return result;
+
+        int top = 0, bottom = mat.size() - 1;
+        int left = 0, right = mat[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; ++i) {
+                result.push_back(mat[top][i]);
             }
-            if(count==totcount)break;
-            up++;
-            for(int i=up;i<=down;i++)
-            {
-                ans.push_back(matrix[i][right]);
-                count++;
+            ++top;
+
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(mat[i][right]);
             }
-            if(count==totcount)break;
-            right--;
-            for(int j=right;j>=left;j--)
-            {
-                ans.push_back(matrix[down][j]);
-                count++;
+            --right;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; --i) {
+                    result.push_back(mat[bottom][i]);
+                }
+                --bottom;
             }
-            if(count==totcount)break;
-            down--;
-            for(int i=down;i>=up;i--)
-            {
-                ans.push_back(matrix[i][left]);
-                count++;
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(mat[i][left]);
+                }
+                ++left;
             }
-            if(count==totcount)break;
-            left++;
         }
-        return ans;
+
+        return result;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
@@ -62,9 +53,10 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        vector<vector<int>> matrix(r);
 
         for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
@@ -75,6 +67,9 @@ int main() {
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
