0ebb5205c4296e9c4b5b3bc83e1b1fddba684f6e
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-01-02 22:41:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        // code here
+        map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            sum += arr[i];
+            
+            int rem = sum-k;
+            
+            if(mp.find(rem) != mp.end()){
+                count += mp[rem];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.countSubarrays(arr, k);
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
