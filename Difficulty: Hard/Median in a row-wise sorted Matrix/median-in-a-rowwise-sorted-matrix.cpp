5a7b94619a0c525b83bd4de8db261384c7d13b9f
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-01-20 16:33:32 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+private:
+    int upperBound(vector<int>& arr, int x, int m) {
+        int low = 0, high = m - 1;
+        int ans = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    
+    int countSmallEqual(vector<vector<int>>& matrix, int n, int m, int x) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += upperBound(matrix[i], x, m);
+        }
+        return cnt;
+    }
+    
+public:
+    int median(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = INT_MAX, high = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            low = min(low, matrix[i][0]);
+            high = max(high, matrix[i][m - 1]);
+        }
+        int req = (n * m) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int smallEqual = countSmallEqual(matrix, n, m, mid);
+            if (smallEqual <= req) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for (int i = 0; i < r; ++i)
+            for (int j = 0; j < c; ++j)
+                cin >> matrix[i][j];
+        Solution obj;
+        int ans = -1;
+        ans = obj.median(matrix);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
