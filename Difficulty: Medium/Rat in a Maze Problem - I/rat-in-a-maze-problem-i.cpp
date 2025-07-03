cb1ac7224485669cd7ec1619d5a149ce0f775fa0
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-03 17:48:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,73 +1,59 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
-  public:
-    vector<string> ans;
-    void find(vector<vector<int>> &mat, int i, int j, int n, vector<vector<bool>> vis, string st){
-        if(vis[i][j] || mat[i][j]==0)
-            return;
-        if(i==n-1 && j==n-1){
-            ans.push_back(st);
+public:
+    int n;
+    void solve(vector<vector<int>>& grid, int i, int j, string& path, vector<string>& ans, vector<vector<int>>& vis) {
+        if(i == n - 1 && j == n - 1) {
+            ans.push_back(path);
             return;
         }
-        vis[i][j] = true;
-        if(i<n-1){
-            find(mat, i+1, j, n, vis, st+"D");
+
+        vis[i][j] = 1;
+
+        // Down
+        if(i + 1 < n && grid[i + 1][j] == 1 && !vis[i + 1][j]) {
+            path += 'D';
+            solve(grid, i + 1, j, path, ans, vis);
+            path.pop_back();
         }
-        if(j<n-1){
-            find(mat, i, j+1, n, vis, st+"R");
+
+        // Up
+        if(i - 1 >= 0 && grid[i - 1][j] == 1 && !vis[i - 1][j]) {
+            path += 'U';
+            solve(grid, i - 1, j, path, ans, vis);
+            path.pop_back();
         }
-        if(i>0){
-            find(mat, i-1, j, n, vis, st+"U");
+
+        // Right
+        if(j + 1 < n && grid[i][j + 1] == 1 && !vis[i][j + 1]) {
+            path += 'R';
+            solve(grid, i, j + 1, path, ans, vis);
+            path.pop_back();
         }
-        if(j>0){
-            find(mat, i, j-1, n, vis, st+"L");
+
+        // Left
+        if(j - 1 >= 0 && grid[i][j - 1] == 1 && !vis[i][j - 1]) {
+            path += 'L';
+            solve(grid, i, j - 1, path, ans, vis);
+            path.pop_back();
         }
+
+        vis[i][j] = 0; // unmark
     }
-    vector<string> findPath(vector<vector<int>> &mat) {
-        // Your code goes here
-        if(mat[0][0]==0)
-            return {"-1"};
-        int n = mat.size();
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
-        find(mat, 0, 0, n, vis, "");
+
+    vector<string> ratInMaze(vector<vector<int>> &grid) {
+        n = grid.size();
+
+        vector<string> ans;
+        if(grid[0][0] == 0 || grid[n - 1][n - 1] == 0) return ans;
+
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+
+        string path = "";
+
+        solve(grid, 0, 0, path, ans, vis);
+        
+        sort(ans.begin(), ans.end());
+
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> m[i][j];
-            }
-        }
-        Solution obj;
-        vector<string> result = obj.findPath(m);
-        sort(result.begin(), result.end());
-        if (result.size() == 0)
-            cout << -1;
-        else
-            for (int i = 0; i < result.size(); i++)
-                cout << result[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
