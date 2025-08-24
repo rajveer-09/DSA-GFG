ee1f00b902cb5a4bbb07a824f54e33e55c875a6a
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-24 14:08:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
@@ -1,63 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  public:
+public:
     int maxLen(vector<int> &arr) {
-        // Your code here
-        unordered_map<int,int>mp;
-        int sum=0;
-        int maxlen=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==0)sum+=-1;
-            else sum+=1;
+        int n = arr.size();
+        
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > 0) sum++;
+            else sum--;
             
-            if(sum==0){
-                maxlen=i+1;
+            if (mp.count(sum)) {
+                maxLen = max(maxLen, i - mp[sum]);
+            } else {
+                mp[sum] = i;
             }
-            
-            if(mp.find(sum)!=mp.end()){
-                maxlen=max(maxlen,i-mp[sum]);
-            }
-            else{
-                mp[sum]=i;
-            }
-            
         }
-        return maxlen;
+        
+        return maxLen;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int T;
-    cin >> T;
-    cin.ignore(); // To ignore the newline character after reading T
-
-    while (T--) {
-        string line;
-        getline(cin, line); // Read the whole line for the array
-
-        // Convert the line into an array of integers
-        stringstream ss(line);
-        vector<int> a;
-        int num;
-        while (ss >> num) {
-            a.push_back(num);
-        }
-
-        // Create the solution object
-        Solution obj;
-
-        // Call the maxLen function and print the result
-        cout << obj.maxLen(a) << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
